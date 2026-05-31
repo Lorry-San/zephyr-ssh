f63758fa12fb3009035eaa52251450bd75cff4a4
Minis <minis@example.local>
2026-05-31 12:49:40 +0800

    fix: avoid rdp login stalls from sound and dynamic resize

--- public/guacamole.js
+++ public/guacamole.js
@@ -1,5 +1,5 @@
 const $ = (sel) => document.querySelector(sel);
-const GUAC_CLIENT_VERSION = '2026-05-31.23-rdp-input-resize-stable';
+const GUAC_CLIENT_VERSION = '2026-05-31.24-rdp-login-stable';
 console.info('[guac-client]', 'script loaded', { version: GUAC_CLIENT_VERSION });
 
 const statusDot = $('#statusDot');
